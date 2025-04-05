de7b63be7c46892bfba05b8969a825ab8ef93028
amerHazaa <31358138+amerHazaa@users.noreply.github.com>
2025-04-05 04:47:57 +0300

    Delete includes directory

--- includes/class-loader.php
+++
@@ -1,9 +0,0 @@
-<?php
-spl_autoload_register(function ($class) {
-    if (strpos($class, 'GRE_') === 0) {
-        $file = plugin_dir_path(__FILE__) . 'class-' . strtolower(str_replace('GRE_', '', $class)) . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-        }
-    }
-});
